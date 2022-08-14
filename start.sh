d249f4d546ab67c7bbaa9080d90b7ebb14694a36
Juliana Repossi <julianarepossi@hotmail.com>
2022-08-14 00:15:52 -0300

    Roteamento estático completo e funcionando perfeitamente - testes foram aplicados

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RTR=rtr.jar
+
+tmux new-session -d -s roteadores 'java -jar '$RTR' routersc r1-hw.txt r1-sw.txt'
+tmux split-window -v -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc r2-hw.txt r2-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc r3-hw.txt r3-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc r4-hw.txt r4-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc r5-hw.txt r5-sw.txt' ENTER;
+tmux select-layout tiled;
+tmux a;
